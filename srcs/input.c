4549288f12be77ba85178247a5476663b27c2aeb
kwevan <kyg972@msn.com>
2020-10-08 14:17:36 +0200

    secure input.c fonctions and add ft_strdel

--- srcs/input.c
+++ srcs/input.c
@@ -6,15 +6,13 @@
 /*   By: kgouacid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/07 14:35:26 by kgouacid          #+#    #+#             */
-/*   Updated: 2020/10/07 16:19:18 by kgouacid         ###   ########.fr       */
+/*   Updated: 2020/10/08 14:13:22 by kgouacid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//rechecker  les mallocs
-
-char	*replace_tilde(char *home, char *input, int i)
+char	*replace_tilde(t_minishell *mini, char *home_path, int i)
 {
 	char	*start;
 	char	*end;
@@ -23,36 +21,44 @@ char	*replace_tilde(char *home, char *input, int i)
 	start = NULL;
 	end = NULL;
 	res = NULL;
-	start = ft_strndup(input, i);
-	end = ft_strjoin(home, input + i + 1);
+	if (!home_path)
+		return (mini->input);
+	if (!(start = ft_strndup(mini->input, i)))
+		exit_shell(mini);
+	if (!(end = ft_strjoin(home_path, mini->input + i + 1)))
+	{
+		ft_strdel(&start);
+		exit_shell(mini);
+	}
 	res = ft_strjoin(start, end);
-	free(input);
-	free(start);
-	free(end);
+	ft_strdel(&start);
+	ft_strdel(&end);
+	ft_strdel(&mini->input);
+	if (!res)
+		exit_shell(mini);
 	return (res);
 }
 
-//cassé avec les quotes
 void	parse_input(t_minishell *mini, char **env)
 {
 	int		i;
 	char	*ret;
+	t_quotes quotes;
 
+	ft_bzero(&quotes, sizeof(t_quotes));
 	ret = NULL;
 	i = 0;
 	while (mini->input[i])
 	{
-		if (mini->input[i] == '~')
+		if ( !ft_quote_open(&quotes, mini->input[i]) && (mini->input[i] == '~'))
 		{
-			ret = replace_tilde(ft_get_envv(env, "HOME"), mini->input, i);
+			ret = replace_tilde(mini, ft_get_envv(env, "HOME"), i);
 			mini->input = ret;
+			parse_input(mini, env);
 			return ;
 		}
 		i++;
 	}
-	ret = ft_strdup(mini->input);
-	free(mini->input);
-	mini->input = ret;
 }
 
 void	get_input(t_minishell *mini)
@@ -78,5 +84,4 @@ void	get_input(t_minishell *mini)
 		exit_shell(mini);
 	}
 	parse_input(mini, mini->env);
-	ft_putstr_fd(mini->input, 1);
 }
