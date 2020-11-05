6bbb52c06b5ecad2a0492dcc3065273bf0f8e48a
kwevan <kyg972@msn.com>
2020-11-05 21:07:26 +0100

    secure getcwd

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: afoulqui <afoulqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 10:40:22 by afoulqui          #+#    #+#             */
-/*   Updated: 2020/11/05 17:57:47 by afoulqui         ###   ########.fr       */
+/*   Updated: 2020/11/05 21:06:41 by kwe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,24 +26,27 @@ static char		*get_path_cd(t_minishell *minishell, char *cmd)
 void			ft_cd(t_minishell *minishell, char **cmd)
 {
 	int		argc;
-	char	cwd[4096];
+	char	*cwd;
 	char	*path;
 
 	argc = ft_countstrarr(cmd);
+	cwd = NULL;
 	path = get_path_cd(minishell, cmd[1]);
 	if (argc > 2)
-	{
 		ft_putstr_fd("cd : too many arguments\n", STDOUT_FILENO);
-	}
 	else if (chdir(path) < 0)
 	{
-		ft_putstr_fd("cd : no such file or directory : ", STDOUT_FILENO);
-		ft_putstr_fd(cmd[1], STDOUT_FILENO);
+		ft_putstr_fd("cd : ", 1);
+		ft_putstr_fd(strerror(errno), 1);
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	}
 	else
 	{
-		getcwd(cwd, 4096);
+		if (!(cwd = getcwd(NULL, 0)))
+		{
+			ft_putstr_fd(strerror(errno), 1);
+			return ;
+		}
 		path = ft_strjoin("PWD=", cwd);
 		add_env(minishell, path);
 	}
