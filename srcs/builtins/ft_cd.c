7815bdc57be31a6e19c16e3be95f8c7ec474707b
kwevan <kyg972@msn.com>
2020-11-05 21:10:00 +0100

    stdout fileno

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: afoulqui <afoulqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/29 10:40:22 by afoulqui          #+#    #+#             */
-/*   Updated: 2020/11/05 21:06:41 by kwe              ###   ########.fr       */
+/*   Updated: 2020/11/05 21:09:40 by kwe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +36,15 @@ void			ft_cd(t_minishell *minishell, char **cmd)
 		ft_putstr_fd("cd : too many arguments\n", STDOUT_FILENO);
 	else if (chdir(path) < 0)
 	{
-		ft_putstr_fd("cd : ", 1);
-		ft_putstr_fd(strerror(errno), 1);
+		ft_putstr_fd("cd : ", STDOUT_FILENO);
+		ft_putstr_fd(strerror(errno), STDOUT_FILENO);
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	}
 	else
 	{
 		if (!(cwd = getcwd(NULL, 0)))
 		{
-			ft_putstr_fd(strerror(errno), 1);
+			ft_putstr_fd(strerror(errno), STDOUT_FILENO);
 			return ;
 		}
 		path = ft_strjoin("PWD=", cwd);
