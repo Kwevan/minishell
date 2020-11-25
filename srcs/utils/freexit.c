9fe2e187a7a760b3d21620d3360aebfff89e07f7
Amélie Foulquier <amelie.foulquier@gmail.com>
2020-11-25 17:00:02 +0100

    added free curr_dir & commands in exit_shell

--- srcs/utils/freexit.c
+++ srcs/utils/freexit.c
@@ -6,7 +6,7 @@
 /*   By: afoulqui <afoulqui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 17:07:44 by afoulqui          #+#    #+#             */
-/*   Updated: 2020/11/20 15:04:48 by afoulqui         ###   ########.fr       */
+/*   Updated: 2020/11/25 16:58:00 by afoulqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,12 @@ void	exit_shell(t_minishell *minishell, int status)
 {
 	if (minishell->env)
 		ft_freestrarr(minishell->env);
+	if (minishell->commands)
+		ft_freestrarr(minishell->commands);
 	if (minishell->input)
 		free(minishell->input);
+	if (minishell->curr_dir)
+		free(minishell->curr_dir);
 	if (minishell)
 		minishell = NULL;
 	exit(status);
